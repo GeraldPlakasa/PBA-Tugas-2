6f0fe50eb270598bd7074e9aba64c929b2f46e7b
fidyahptr <fidyahputri15@gmail.com>
2022-03-06 21:23:59 +0700

    update 2a
    Update 2a

--- Tugas2-a.py
+++ Tugas2-a.py
@@ -1,59 +1,32 @@
-import os
-from tkinter import *
+import nltk.tokenize.punkt
+import nltk.data
 import pickle
-import codecs
-import nltk
-# nltk.download('punkt')
+from timeit import default_timer as timer
 
-struct = Tk()
-struct.geometry("254x250")
-struct.title("Pemrosesan Bahasa Alami")
-label = Label(struct, text="Algoritma Punkt", bg="gray",
-              fg="white", font=("Comic Sans MS", 20, "bold"))
-label.place(relx=.5, rely=.2, anchor='center')
-struct.config(background="gray")
-text = StringVar()
+cleaning_file = f'1500.txt'
 
 
-def btnPunkt():
-    print('─' * 100)
+def punkt(file):
     tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
-    f = codecs.open("1000.txt", "r", "utf8").read()
-    tokenizer.train(f)
-    out = open("indonesian.pickle", "wb")
-    pickle.dump(tokenizer, out)
-    out.close()
-    seg_kalimat = nltk.data.load('indonesian.pickle')
-    teks = seg_kalimat.tokenize(f)
+    tokenizer.train(file)
+    with open('indonesian.pickle', "wb") as out:  # membuat file pickle
+        pickle.dump(tokenizer, out)
+        out.close()
+    sentence_segment = nltk.data.load('indonesian.pickle')
+    text_sentence = sentence_segment.tokenize(file)
 
-    # menghapus nilai kosong pada list
-    teks = list(filter(None, teks))
-
-    # membuat file hasil / jika sudah ada di replace
-    try:
-        f2 = open("hasil_punkt.txt", "x")
-        f2.close()
-    except Exception as e:
-        os.remove("hasil_punkt.txt")
-        f2 = open("hasil_punkt.txt", "x")
-        f2.close()
-
-    for punktz in teks:
-        # rename multiple \n to space
-        punktz = punktz.replace('\n', ' ')
-        # hapus mutiple space
-        punktz = re.sub(' +', ' ', punktz)
-        print(punktz.strip())
-        try:
-            f3 = open("hasil_punkt.txt", "a")
-            f3.write(punktz.strip()+"\n\n")
-            f3.close()
-        except Exception as e:
-            print(e)
+    file_after_segmentation = f'file_punkt.txt'
+    with open(file_after_segmentation, 'w') as f:
+        for sentence in text_sentence:
+            f.write(sentence.strip())
+            f.write('\n')
+    print(f'Result: success! file_punkt.txt. saved')
 
 
-button = Button(struct, text="Punkt", font=(
-    "Times", 10, "bold"), width=20, bd=2, command=btnPunkt)
-button.place(relx=.5, rely=.5, anchor='center')
-
-struct.mainloop()
+# Main Program
+start = timer()
+with open(cleaning_file, 'r') as r:
+    openFile = r.read()
+punkt(openFile)
+end = timer()
+print(f"\nprocess finished {end-start} seconds")
